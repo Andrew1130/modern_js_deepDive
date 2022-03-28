8b4572d91d2f1ef4ae2bd04f9630d3ea37a2ddcd
Andrew1130 <yhyh273@gmail.com>
2022-03-28 13:57:50 +0900

    pp.100~136 (8.3 ~ 10.9)
    반복문, 타입변환, 단축평가, 객체, 프로퍼티, 메서드

--- index.js
+++ index.js
@@ -1,30 +1,15 @@
-var serialNum = '0004';
-var productName;
+// var obj = {
+//   'name' : 'Lee',
+//   'sayHi' : function(){
+//     console.log('hi! '+this.name);
+//   }
+// }
 
-switch(serialNum) {
-  case '0001': productName = 'Cpn0001'
-  break
-  case '0002': productName = 'Cpn0002'
-  break
-  case '0003': productName = 'Cpn0003'
-  break
-  case '0004': productName = 'Cpn0004'
-  break
-  case '0005': productName = 'Cpn0005'
-  break
-  case '0006': productName = 'Cpn0006'
-  break
-  case '0007': productName = 'Cpn0007'
-  break
-  case '0008': productName = 'Cpn0008'
-  break
-  case '0009': productName = 'Cpn0009'
-  break
-  case '0010': productName = 'Cpn0010'
-  break
-  case '0011': productName = 'Cpn0011'
-  break
-  default: productName = 'non-exist'
+var obj = {
+  'name' : 'Lee',
+  sayHi() {
+    console.log('hi! '+this.name);
+  }
 }
 
-console.log(productName)
+obj.sayHi();
